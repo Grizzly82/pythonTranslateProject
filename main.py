16bbb77ec3fe696941bf8f2afc8932e2b31b3da1
Cris Vizcaino <cris.vizcaino@live.com>
2020-08-21 20:07:15 -0700

    Changed readline() to read(), no longer need the join function. Output is cleaner

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ translator = Translator(to_lang="es")
 try:
     with open('translateText.txt', mode='r+') as my_file:
        # print(my_file.readlines())
-        myFile = my_file.readlines()
-        newText = " ".join(myFile)
-        translation = translator.translate(newText)
+        myFile = my_file.read()
+        #newText = " ".join(myFile)
+        translation = translator.translate(myFile)
         print(translation)
 except FileNotFoundError as err:
     print("This file was not found or does not exist")
