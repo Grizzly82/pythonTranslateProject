49a527195421c4a90e2676ca10102a0757361e59
Cris Vizcaino <cris.vizcaino@live.com>
2020-08-21 20:22:45 -0700

    added a write function to output data to an external file

--- main.py
+++ main.py
@@ -7,10 +7,10 @@ translator = Translator(to_lang="es")
 
 try:
     with open('translateText.txt', mode='r+') as my_file:
-       # print(my_file.readlines())
         myFile = my_file.read()
-        #newText = " ".join(myFile)
         translation = translator.translate(myFile)
-        print(translation)
+        with open('.test-lang.txt', mode='w') as new_file:
+            new_file.write(translation)
+            new_file.close()
 except FileNotFoundError as err:
     print("This file was not found or does not exist")
