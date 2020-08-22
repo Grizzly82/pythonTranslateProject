6eab53c9e9191b8766f2e707a98b1715fde2ae9b
Cris Vizcaino <cris.vizcaino@live.com>
2020-08-21 20:36:29 -0700

    changed the encoding to include utf-8

--- main.py
+++ main.py
@@ -9,8 +9,9 @@ try:
     with open('translateText.txt', mode='r+') as my_file:
         myFile = my_file.read()
         translation = translator.translate(myFile)
-        with open('.test-lang.txt', mode='w') as new_file:
+        with open('.test-lang.txt', mode='w+', encoding="utf-8") as new_file:
             new_file.write(translation)
             new_file.close()
+            print(translation)
 except FileNotFoundError as err:
     print("This file was not found or does not exist")
